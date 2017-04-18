d0d88c9e50606ff73eabf35b7b839a17068543e3
Evgeniy Grabets <didrash@mail.ru>
2017-04-18 11:22:03 +0300

    change to entity body

--- src/main/java/evg/test/controller/DataController.java
+++ src/main/java/evg/test/controller/DataController.java
@@ -3,6 +3,9 @@ package evg.test.controller;
 import evg.test.exception.RestException;
 import evg.test.util.Ajax;
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -13,26 +16,25 @@ public class DataController extends ExceptionHandlerController {
 
     private static final Logger LOG = Logger.getLogger(DataController.class);
 
-    @RequestMapping(value = "/getData",  method = RequestMethod.GET)
-    public @ResponseBody
-    Map<String, Object> getData(@RequestParam(value = "category", required = false) String[] category,
-                                @RequestParam(value = "tags", required = false) String[] tags,
-                                @RequestParam(value = "studio", required = false) String studio[],
-                                @RequestParam(value = "publishTime", required = false, defaultValue = "relevancy") String publishTime,
-                                @RequestParam(value = "promotedIds", required = false) String promotedIds []) throws RestException {
+    @RequestMapping(value = "/getData", method = RequestMethod.GET)
+    public ResponseEntity String(@RequestParam(value = "category", required = false) String[] category,
+                                 @RequestParam(value = "tags", required = false) String[] tags,
+                                 @RequestParam(value = "studio", required = false) String studio[],
+                                 @RequestParam(value = "publishTime", required = false, defaultValue = "relevancy") String publishTime,
+                                 @RequestParam(value = "promotedIds", required = false) String promotedIds[]) throws RestException {
 
         System.out.println("category " + Arrays.toString(category));
         System.out.println("tags " + Arrays.toString(tags));
-        System.out.println("studio " +  Arrays.toString(studio));
+        System.out.println("studio " + Arrays.toString(studio));
         System.out.println("publishTime " + publishTime);
-        System.out.println("promotedIds " +  Arrays.toString(promotedIds));
+        System.out.println("promotedIds " + Arrays.toString(promotedIds));
 
-        try {
-            return Ajax.emptyResponse();
-        } catch (Exception e) {
-            LOG.error("Cant execute //getData", e);
-            throw new RestException(e);
-        }
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("HeaderKey", "HeaderData");
+        return new ResponseEntity<>(
+                "<i>This is</i> the <h2>Page value</h2> (ResponseBody)", responseHeaders,
+                HttpStatus.OK);
+
     }
 
 }
